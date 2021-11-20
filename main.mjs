1bd07595356c58bc32a7112ccb2b60ff1d9f5965
Jason Kuhrt <jasonkuhrt@me.com>
2021-11-20 19:17:18 +0000

    feat: more cases captured

--- main.mjs
+++ main.mjs
@@ -1,12 +1,14 @@
+const log = console.log
+
 import a from 'a'
-console.log(a)
+log(a)
 
 // Cannot import "b" because no export specified for main
 // Even though "b" package has "main" defined, "exports" causes it to be ignored by any Node version that understands "exports"
 // import b from 'b'
 
 import * as b from 'b/custom'
-console.log(b)
+log(b)
 
 // This will fail because export spec is missing extension. Exports file paths do not imply a .js extension
 // import c from 'c/custom'
@@ -16,7 +18,7 @@ console.log(b)
 // import dir from 'd/dir'
 
 import * as e from 'e/dir'
-console.log(e)
+log(e)
 
 // This will fail. Needs to include extension
 // Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/jasonkuhrt/foobar/node_modules/f/dir/one' imported from /Users/jasonkuhrt/foobar/foo.mjs
@@ -27,21 +29,21 @@ console.log(e)
 // import * as f from 'f/dir-star'
 
 import * as f1 from 'f/dir-star/one.mjs'
-console.log(f1)
+log(f1)
 
 import * as g1 from 'g/dir-star-sans-extension/one'
-console.log(g1)
+log(g1)
 
 import * as h1 from 'h/dir-star-except/one'
-console.log(h1)
+log(h1)
 
 // This works. Cannot exclude files.
 import * as h2 from 'h/dir-star-except/two'
-console.log(h2)
+log(h2)
 
 // This works. * does not pause at /
 import * as hNested1 from 'h/dir-star-except/nested1/one'
-console.log(hNested1)
+log(hNested1)
 
 // This will fail. It has been excluded.
 // Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './dir-star-except/nested2/one' is not defined by "exports" in /Users/jasonkuhrt/foobar/node_modules/h/package.json imported from /Users/jasonkuhrt/foobar/foo.mjs
@@ -53,7 +55,7 @@ console.log(hNested1)
 
 // This works. Wildcard for file names.
 import * as i2 from 'i/string-match2'
-console.log(i2)
+log(i2)
 
 // This will fail. Anything after first 2 has been excluded
 // import * as i22 from 'i/string-match22'
@@ -61,30 +63,30 @@ console.log(i2)
 // Needs flag --experimental-json-modules
 // https://nodejs.org/api/esm.html#esm_experimental_json_modules
 import j1 from 'j/multi-ext1.json'
-console.log(j1)
+log(j1)
 
 import * as j2 from 'j/multi-ext2.mjs'
-console.log(j2)
+log(j2)
 
 // This works. But odd. A wildcard import specifier is mapped to a singular file.
 import * as k1 from 'k/wildcard/one'
 import * as k2 from 'k/wildcard/two'
 import * as k3 from 'k/wildcard/three'
-console.log(k1)
-console.log(k2)
-console.log(k3)
-console.log(k1 === k2, k1 === k3)
+log(k1)
+log(k2)
+log(k3)
+log(k1 === k2, k1 === k3)
 
 // This will fail. File spec Wildcard is treated as literal when there is no wildcard import specifier.
 // Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/jasonkuhrt/foobar/node_modules/l/*.mjs' imported from /Users/jasonkuhrt/foobar/foo.mjs
 // import * as l from 'l/singular'
 
 import * as l2 from 'l/wildcard-literal'
-console.log(l2)
+log(l2)
 
 // Will work. Subpath dot import specifier is taken literally. Only root dot has special semantics.
 import * as m from 'm/sub-dot/.'
-console.log(m)
+log(m)
 
 // Will not work. Module specifier of just "*" does not permit importing in any way.
 // import * as n from 'n'
@@ -101,7 +103,7 @@ console.log(m)
 // This will work. The trailing dot given by consumer appended with the wildcard suffix leads to a complete file path.
 // NOTE This is quite esoteric.
 import * as q from 'q/one.'
-console.log(q)
+log(q)
 
 // import specifier infix
 
@@ -109,7 +111,7 @@ console.log(q)
 import * as r1 from 'r/f*o'
 import * as r2 from 'r/foo'
 import * as r3 from 'r/foooooooooooooooooooooooooooooooooooooooooooooooo'
-console.log(r1 === r2, r1 === r3)
+log(r1 === r2, r1 === r3)
 
 // This will fail. "fo" does not match "f*o"
 // import * as r4 from 'r/fo'
@@ -128,4 +130,36 @@ console.log(r1 === r2, r1 === r3)
 
 // This will work. Node skips the null value in the array.
 import * as u from 'u'
-console.log(u)
+log(u)
+
+// This will work. Node recurses resolution through each array item.
+import * as v from 'v'
+log(v)
+
+// This will work. Node recursively unwraps target arrays. But it does not seem to pass Package.json schema validation.
+import * as v1 from 'v/one'
+log(v1)
+
+import * as vArrayOne from 'v/array/one'
+log(vArrayOne)
+
+// Can negate a subpath with null inside an array.
+// This will (should) fail.
+// import * as vArrayNull from 'v/array/null'
+// log(vArrayNull)
+
+// Can have target conditionals
+import * as vTwo from 'v/two'
+log(vTwo)
+
+// Can negate a subpath with null inside a target conditional
+// import * as vThree from 'v/three'
+// log(vThree)
+
+// Can negate a subpath with null inside array inside a target conditional
+// import * as vFour from 'v/four'
+// log(vFour)
+
+// Nested conditionals work
+import * as vFive from 'v/five'
+log(vFive)
